44a5a3f81388537ea687e5f8be22b9b11e04b33d
Ornella Mesina G. <mesinaornella@gmail.com>
2026-06-02 14:57:28 +0200

    error del gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-implementation("androidx.compose.material3:material3:1.2.1")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
